4e23ba6b7027adf91e3f28b5c69f542205c0f518
FoteinosMerg <foteinomerg@protonmail.com>
2019-07-30 09:01:34 +0300

    minors

--- crypto/elgamal.py
+++ crypto/elgamal.py
@@ -1,113 +1,128 @@
 from abc import ABCMeta, abstractmethod
 
 
-class ElGamalCrypto(metaclass=ABCMeta):
+class ElGamalCrypto(object, metaclass=ABCMeta):
 
-    @abstractmethod
+    __slots__ = (
+        '__system',
+        '__schnorr_proof',
+        '__schnorr_verify',
+        '__chaum_pedersen_proof',
+        '__chaum_pedersen_verify',
+        '__keygen'
+    )
+
+
     def __init__(self, *config):
         """
         """
         try:
-            ElGamalCrypto.validate_config(*config)
-        except: # Put here exceptions
+            system = ElGamalCrypto.generate_system(*config)
+        except WrongCryptoError:
             raise
-
-        system = generate_system(*config)
 
         try:
             ElGamalCrypto.validate_system(system)
-        except: # Put here exceptions
+        except (WrongCryptoError, WeakCryptoError):
             raise
 
         self.__system = system
         ElGamalCrypto.load_primitives(system)
 
-    @staticmethod
-    @abstractmethod
-    def validate_config():
-        """
-        """
 
     @staticmethod
-    @abstractmethod
-    def generate_system():
+    def load_primitives(system):
         """
         """
-
-    @staticmethod
-    @abstractmethod
-    def validate_system():
-        """
-        """
+        ElGamalCrypto.make_schnorr_proof(system)
+        ElGamalCrypto.make_schnorr_verify(system)
+        ElGamalCrypto.make_chaum_pedersen_proof(system)
+        ElGamalCrypto.make_chaum_pedersen_verify(system)
+        ElGamalCrypto.make_keygen(system)
 
     @property
     def system():
         return self.__system
 
-    @staticmethod
-    @abstractmethod
-    def load_primitives(system):
-        """
-        """
 
-    @abstractmethod
-    def schnorr_proof(self, secret, public, *extras):
-        """
-        """
+# --------------------------------- Interface ---------------------------------
 
-
-    @abstractmethod
-    def schnorr_verify(self, proof, public, *extras):
-        """
-        """
-
-
-    @abstractmethod
-    def chaum_pedersen_proof(u, v, w, z):
-        """
-        """
-
-
-    @abstractmethod
-    def chaum_pedersen_verify(u, v, w, proof):
-        """
-        """
-
-
-    # @abstractmethod
+    # def schnorr_proof(self, secret, public, *extras):
+    #     """
+    #     """
+    #     return self.__schnorr_proof(self, secret, public, *extras)
+    #
+    #
+    # def schnorr_verify(self, proof, public, *extras):
+    #     """
+    #     """
+    #     return self.__schnorr_verify(self, proof, public, *extras)
+    #
+    #
+    # def chaum_pedersen_proof(u, v, w, z):
+    #     """
+    #     """
+    #     return self.__chaum_pedersen_proof(u, v, w, z)
+    #
+    #
+    # def chaum_pedersen_verify(u, v, w, proof):
+    #     """
+    #     """
+    #     return self.__chaum_pedersen_verify(u, v, w, proof)
+    #
+    #
     # def keygen(private_key=None, schnorr=False):
     #     """
     #     """
-    #
-    # @abstractmethod
-    # def validate_key(public_key, proof):
-    #     """
-    #     """
-    #     pass
-    #
-    # @abstractmethod
-    # def sign_element():
-    #     """
-    #     """
-    #     pass
-    #
-    # @abstractmethod
-    # def verify_element_signature():
-    #     """
-    #     """
-    #     pass
-    #
-    # @abstractmethod
-    # def sign_message():
-    #     """
-    #     """
-    #
-    # @abstractmethod
-    # def verify_message_signature():
-    #     """
-    #     """
-    #
-    # @abstractmethod
-    # def encrypt(element, public_key, randomness=None):
-    #     """
-    #    """
+    #     return self.__keygen(private_key, schnorr)
+
+
+# ------------------------------ Abstract methods ------------------------------
+
+    @staticmethod
+    @abstractmethod
+    def generate_system(*config):
+        """
+        """
+
+    @staticmethod
+    @abstractmethod
+    def extract_parameters(system):
+        """
+        """
+
+    @staticmethod
+    @abstractmethod
+    def validate_system(system):
+        """
+        """
+
+    @staticmethod
+    @abstractmethod
+    def make_schnorr_proof(system):
+        """
+        """
+
+    @staticmethod
+    @abstractmethod
+    def make_schnorr_verify(system):
+        """
+        """
+
+    @staticmethod
+    @abstractmethod
+    def make_chaum_pedersen_proof(system):
+        """
+        """
+
+    @staticmethod
+    @abstractmethod
+    def make_chaum_pedersen_proof(system):
+        """
+        """
+
+    @staticmethod
+    @abstractmethod
+    def make_keygen(system):
+        """
+        """
--- crypto/modprime.py
+++ crypto/modprime.py
@@ -1,4 +1,140 @@
-from .elgamal import ElGamalCryptosystem
+from .elgamal import ElGamalCrypto
+from .exceptions import WrongCryptoError
+from .algebra import number, _add, _mul, _divmod, _mod, _pow, _inv
 
-class ModPrimeCrypto(ElGamalCryptosystem):
-    pass
+class ModPrimeCrypto(object):#(ElGamalCrypto):
+
+    def __init__(self, modulus, root_order, element):
+
+        # config = [modulus, root_order, element]
+        super().__init__(modulus, root_order, element)
+        # try:
+        #     system = ModPrimeCrypto.generate_system(*config)
+        # except WrongCryptoError:
+        #     raise
+        #
+        # try:
+        #     ModPrimeCrypto.validate_system(system)
+        # except (WrongCryptoError, WeakCryptoError):
+        #     raise
+        #
+        # self.system = system
+        # ModPrimeCrypto.load_primitives(system)
+
+
+    @staticmethod
+    def generate_system(modulus, root_order, element):
+        """
+        """
+
+        p  = modulus
+        r  = root_order
+        g0 = element
+
+        if p <= 2 or not number.isPrime(p):
+            e = 'Provided modulus is not an odd prime'
+            raise WrongCryptoError(e)
+
+        nr_elements = p - 1
+
+        if g0 < 2 or g0 > nr_elements - 1:
+            e = 'Provided element does not belong to the multiplicative group'
+
+        q, s = _divmod(nr_elements, r)  # q = (p - 1)/r
+
+        if s != 0:
+            e = 'Provided order does not divide the multiplicative group\'s order'
+            raise WrongCryptoError(e)
+
+        if not number.isPrime(q):
+            e = 'Order of the requested group is not prime'
+            raise WrongCryptoError(e)
+
+        g = _pow(g0, r, p)  # g = g0 ^ r
+
+        if g == 1:
+            # Algebraic fact: given 1 < x < p for a smooth prime p and 1 < r < p - 1 with
+            # r | p - 1, then x ^ (p - 1)/r generates the r-subgroup of Z^*_p if it is != 1
+            e = 'Provided element cannot yield the requested subgroup\'s generator'
+            raise WrongCryptoError(e)
+
+        return {
+            'modulus': p,
+            'generator': g,
+            'order':q
+        }
+
+
+    @staticmethod
+    def extract_parameters(system):
+        """
+        """
+
+        p = system['modulus']
+        q = system['order']
+        g = system['generator']
+
+        return p, g, q
+
+
+    @staticmethod
+    def validate_system(system):
+        """
+        """
+
+        p, q, g = ModPrimeCrypto.extract_parameters(system)
+
+        if _mod(p, 4) != 3:
+            e = 'Modulus is not 3 mod 4'
+            raise WrongCryptoError(e)
+
+        if not number.isPrime(q):
+            e = 'Order of subgroup is not prime'
+            raise WrongCryptoError(e)
+
+        if _mod(p - 1, q) != 0:
+            e = 'Order does not divide the multiplicative group\'s order'
+            raise WrongCryptoError(e)
+
+        if not 1 < g < p or _pow(g, q, p) != 1:
+            e = 'Generator is not valid'
+            raise WrongCryptoError(e)
+
+        if p.bit_length() < min_mod_bit_size:
+            e = 'Modulus is < %d bits long' % min_mod_bit_size
+            raise WeakCryptoError(e)
+
+        if g.bit_length() < min_gen_bit_size:
+            e = 'Generator is < %d bits long' % min_gen_bit_size
+            raise WeakCryptoError(e)
+
+
+        @staticmethod
+        def make_schnorr_proof(system):
+            """
+            """
+            return 0
+
+        @staticmethod
+        def make_schnorr_verify(system):
+            """
+            """
+            return 1
+
+        @staticmethod
+        def make_chaum_pedersen_proof(system):
+            """
+            """
+            return 2
+
+        @staticmethod
+        def make_chaum_pedersen_proof(system):
+            """
+            """
+            return 3
+
+        @staticmethod
+        def make_keygen(system):
+            """
+            """
+            return 4
