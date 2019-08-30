f88c73ee04c9149249210e73dd6f59cf353352c4
FoteinosMerg <foteinomerg@protonmail.com>
2019-08-30 17:57:24 +0300

    Fixed typo

--- tests/helpers.py
+++ tests/helpers.py
@@ -138,5 +138,3 @@ def _make_ciphers_to_mix(mixnet, election_key, nr_ciphers=12):
         'cipher_collections': []
     }
     return ciphers_to_mix
-
-def make_encrypted_ballots(system, nr_ballots):
